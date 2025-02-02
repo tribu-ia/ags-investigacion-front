633afce80f6a6af78359cd87eebb40152c3a41fa
JamiltonQuintero <transportador1118@gmail.com>
2025-02-01 19:50:22 -0500

    Se agrtega asistente de charlas

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,9 +1,12 @@
 import { SessionProvider } from "@/providers/session-provider";
-
+import N8nChat from "@/components/N8nChat";
 export default function ProtectedLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return <SessionProvider>{children}
+  <N8nChat />
+  </SessionProvider>;
 }
+
