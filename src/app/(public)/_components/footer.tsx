8cf33326536b7559b8d34656edac822829e19a41
Jose Ignacio <josemillaquen12@gmail.com>
2025-02-18 23:45:16 -0300

    feat: enhance footer component with hover effects for social media links

--- src/app/(public)/_components/footer.tsx
+++ src/app/(public)/_components/footer.tsx
@@ -11,7 +11,7 @@ export const FooterLanding = () => {
             Iniciativa de la comunidad de{" "}
             <Link
               href="https://tribuia.org/"
-              className="font-bold text-primary"
+              className="font-bold text-primary hover:underline hover:decoration-wavy decoration-primary"
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -24,9 +24,9 @@ export const FooterLanding = () => {
                 href="https://www.instagram.com/tribuia"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-primary"
+                className="text-primary group"
               >
-                <Instagram className="size-5" />
+                <Instagram className="size-5 group-hover:scale-110 transition-all duration-200" />
               </Link>
             </li>
             <li>
@@ -34,9 +34,9 @@ export const FooterLanding = () => {
                 href="https://chat.whatsapp.com/JX7ctC1P16s2hfoYrcJ0a9"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-primary"
+                className="text-primary group"
               >
-                <WhatsappIcon className="size-5 min-w-5" />
+                <WhatsappIcon className="size-5 min-w-5 group-hover:scale-110 transition-all duration-200" />
               </Link>
             </li>
             <li>
@@ -44,9 +44,9 @@ export const FooterLanding = () => {
                 href="https://www.linkedin.com/company/tribu-ia"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-primary"
+                className="text-primary group"
               >
-                <LinkedinIcon className="size-5 min-w-5" />
+                <LinkedinIcon className="size-5 min-w-5 group-hover:scale-110 transition-all duration-200" />
               </Link>
             </li>
           </ul>
